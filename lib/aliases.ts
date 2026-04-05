7cd5300bd8f07d0095a1bcb10b13bbf1d60226cd
Hemanth Reddy <hemanthreddykoduru2005@gmail.com>
2026-04-05 19:32:56 +0530

    feat: add DC characters to the alias generator list

--- lib/aliases.ts
+++ lib/aliases.ts
@@ -3,19 +3,26 @@
  * Format: [Adjective][Animal] (e.g., "QuickFox", "BrightOwl")
  */
 
-const MARVEL_CHARACTERS = [
+const HERO_CHARACTERS = [
+    // Marvel
     'Iron Man', 'Captain America', 'Thor', 'Hulk', 'Black Widow',
     'Hawkeye', 'Black Panther', 'Scarlet Witch', 'Vision', 'Captain Marvel',
     'Spider-Man', 'Ant-Man', 'Wasp', 'War Machine', 'Falcon',
     'Winter Soldier', 'Loki', 'Valkyrie', 'Mantis', 'Okoye',
     'Shuri', 'Nebula', 'Rocket Raccoon', 'Groot', 'Drax',
-    'Star-Lord', 'Gamora', 'Wong'
+    'Star-Lord', 'Gamora', 'Wong',
+    
+    // DC
+    'Superman', 'Batman', 'Wonder Woman', 'The Flash', 'Green Lantern',
+    'Aquaman', 'Cyborg', 'Martian Manhunter', 'Wonder Girl', 'Green Arrow',
+    'Black Canary', 'Zatanna', 'Shazam', 'Nightwing', 'Hawkman',
+    'Hawkgirl', 'Google', 'Plastic Man', 'Elongated Man', 'Red Tornado'
 ];
 
 /**
  * Generate a random anonymous alias
- * @returns Random alias string from Marvel characters
+ * @returns Random alias string from superhero characters
  */
 export function generateAlias(): string {
-    return MARVEL_CHARACTERS[Math.floor(Math.random() * MARVEL_CHARACTERS.length)];
+    return HERO_CHARACTERS[Math.floor(Math.random() * HERO_CHARACTERS.length)];
 }
