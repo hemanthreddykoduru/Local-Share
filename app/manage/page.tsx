483500a3f32e8ca2b15ad643159b0ff7d6e2f8d9
Hemanth Reddy <hemanthreddykoduru2005@gmail.com>
2026-05-03 11:24:32 +0530

    fix: resolve unclosed div in manage page project list

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -465,6 +465,7 @@ export default function ManageAccountPage() {
                                                         </div>
                                                     </>
                                                 )}
+                                                </div>
                                             </div>
                                         </div>
                                     ))
